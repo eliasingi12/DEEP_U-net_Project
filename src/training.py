2ae35f22ba0070c1f45b65a6d402df5da2ece62f
Brynjars88 <brynjar.sig88@gmail.com>
2018-12-11 15:13:14 +0000

    Started writing unet arcitechture

--- src/training.py
+++ src/training.py
@@ -7,6 +7,7 @@ import numpy as np
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 
+from keras.layers import Input
 from keras.models import Sequential
 from keras.layers.core import Dense
 from keras.optimizers import SGD
@@ -14,8 +15,13 @@ from sklearn.metrics import classification_report
 
 import matplotlib.pyplot as plt
 
+# unet((584,565,1))
 
-def unet():
-    model = Sequential()
-    print('WOOOT')
+def unet(input_shape):
+
+    input_shape = Input(input_shape)
+
+    c1 = keras.layers.Conv2D(input_shape,n_filters=1,kernel_size=3,padding='same',
+                            kernel_initializer='random_uniform')
+    
     return model
